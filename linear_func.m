fb4167348e2973c9a7c508adcc13748bbc587253
Julio Alves <julio@julio-arch.localdomain>
2017-11-17 10:51:04 -0200

    Sistema linear criado

---
+++ linear_func.m
@@ -0,0 +1,19 @@
+function [ y, d_zetta] = linear_func( zetta, u )
+%LINEAR_FUNC Summary of this function goes here
+%   Detailed explanation goes here
+    A = [0 1 0 0;
+        0 bK m*g/MJ 0;
+        0 0 0 1;
+        0 0 -(g/l)*(1+m/MJ)];
+    
+    B = [0; Kr/MJ; 0; -Kr/l];
+    
+    C = [1; 0; 0; 0];
+    
+    D = [0];
+    
+    d_zetta = A*zetta + B*u;
+    
+    y = C*zetta + D*u;
+end
+
--- main.m
+++ main.m
@@ -2,6 +2,15 @@ clear
 close all
 clc
 
+% Dados do sistema
+
+MJ = 1.0731; % [kg]
+m  = 0.209;  % [kg]
+l  = 0.3302; % [m]
+bK = 5.4;    % [N.s/(m²)]
+Kr = 1.0717; % [N/V]
+
+g  = 9.81;   % [m/s²]
 
 % trabalho com Julieras controle =)
 syms teta2 m l P teta M J r R b K teta1 x1 x g
